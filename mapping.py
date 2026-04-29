fc4c57b0c5ae6a7b4412d1ef59bd9fcabfbd4aa5
cltain88 <clta@ucdavis.edu>
2026-04-28 20:32:59 -0700

    Update mapping.py

--- mapping.py
+++ mapping.py
@@ -54,7 +54,7 @@ SENSOR_BORDER_ON  = "#9B59D0"
 
 PLAYER_W = 320
 PDF_W    = 1000
-WIN_H    = 1050
+WIN_H    = 1090
 
 PDF_LIST = [
     ("Datasheet",  "/home/agadkari/srdesign/PDFs/datasheet.pdf"),
